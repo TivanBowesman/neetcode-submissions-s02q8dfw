cef696e683a13c9ef1c8d2becb7d0343c6f73bf9
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-04-15 17:36:34 +0100

    Add: merge-two-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-5.py
@@ -0,0 +1,32 @@
+import heapq
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        #use max heap to add to list
+        heap = []
+        heapq.heapify(heap)
+
+        node = list1
+        while node != None:
+            heapq.heappush(heap, -node.val)
+            node = node.next
+
+        node = list2
+        while node != None:
+            heapq.heappush(heap, -node.val)
+            node = node.next
+
+        prev = None
+        while heap:
+            resNode = ListNode(-heapq.heappop(heap), prev)
+
+            prev = resNode
+        
+        return prev
+            
