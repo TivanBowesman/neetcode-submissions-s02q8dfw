81dafa71b94cc825ba2c6bc0768e759da93739cd
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-05-08 17:53:48 +0100

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var res = new List<List<string>>();
+
+        var map = new Dictionary<string, List<string> >();
+
+        //sort
+        //use sorted as a key - orig as value
+        foreach(string s in strs){
+            var charArr = s.ToCharArray();
+            Array.Sort(charArr);
+            var sorted = new string(charArr);
+
+            if (map.ContainsKey(sorted)){
+                map[sorted].Add(s);
+            }else{
+                map[sorted] = new List<string>();
+                map[sorted].Add(s);
+            }
+        }
+
+        foreach(var s in map.Keys){
+            res.Add(map[s]);
+        }
+        
+        return  map.Values.ToList();
+    }
+}
