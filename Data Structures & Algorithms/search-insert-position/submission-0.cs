f5062933c5ed0381e2d9992d2014a97e0c591a52
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-05-12 22:47:59 +0100

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int l = 0, r = nums.Length - 1; 
+
+        while (l <= r){
+            int mid = l + (r - l);
+            //1, 2,3 ,4 ,5
+            int sum = nums[mid];
+
+            if (sum == target){
+                return mid;
+            }else if (sum < target){
+                l++;
+            }else{
+                r--;
+            }
+
+        }
+
+        return l;
+    }
+}
