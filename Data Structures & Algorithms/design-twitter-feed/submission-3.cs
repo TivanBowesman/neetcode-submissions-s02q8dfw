0bec80d4cf3bda9229bd20ba21884370c9602b8f
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-05-18 18:57:12 +0100

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.cs
@@ -0,0 +1,64 @@
+public class Twitter {
+    private Dictionary<int, List<(int time, int tweetId)>> tweets;
+    private Dictionary<int, HashSet<int>> following;
+    private int time;
+
+    public Twitter() {
+        tweets = new Dictionary<int, List<(int, int)>>();
+        following = new Dictionary<int, HashSet<int>>();
+        time = 0;
+    }
+
+    public void PostTweet(int userId, int tweetId) {
+        if(!tweets.ContainsKey(userId)){
+            //create new list
+            tweets[userId] = new List<(int time, int tweets)>();
+        }
+        //add to list
+        tweets[userId].Add( (time++, tweetId) );
+    }
+
+    public List<int> GetNewsFeed(int userId) {
+        // collect tweets from self + followees
+        var all = new List<(int time, int tweetId)>();
+
+        if (tweets.ContainsKey(userId)){
+            all.AddRange(tweets[userId]);
+        }
+
+        if (following.ContainsKey(userId)) {
+            foreach (var followeeId in following[userId]) {
+                if (followeeId == userId) continue;
+                if (tweets.ContainsKey(followeeId)){
+                    //add all tweets made to the feed
+                    all.AddRange(tweets[followeeId]);
+                }
+            }
+        }
+
+        // sort by most recent
+        return all.OrderByDescending(t => t.time)
+                  .Take(10)
+                  .Select(t => t.tweetId)
+                  .ToList();
+    }
+
+    public void Follow(int followerId, int followeeId) {
+        if (!following.ContainsKey(followerId)){
+            following[followerId] = new HashSet<int>();
+        }
+
+        following[followerId].Add(followeeId);
+    }
+
+    public void Unfollow(int followerId, int followeeId) {
+        //can just call this as it doesnt matter if its in map
+        following[followerId].Remove(followeeId);
+
+    }
+}
+
+// post k-v
+//  userid - (postid, posterId)
+// add to users
+// if unfollowed remove items with item2 == posterId
