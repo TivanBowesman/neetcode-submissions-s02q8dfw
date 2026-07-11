c5ea931e45c5cbee5dd16651532ff4d9d18eeaf4
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-07-11 12:58:02 +0100

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var hashset = new HashSet<int>(nums);
+        return hashset.Count == nums.Length ? false : true;
+    }
+}
