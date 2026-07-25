6095ca1886427bb50134de1e63089249e2065805
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-07-25 12:17:23 +0100

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var arr = new List<int>(k);
+
+        var map = new Dictionary<int,int>();
+        var gb = nums.GroupBy(i => i)
+        .OrderByDescending(i => i.Count())
+        .Select(i => i.Key)
+        .Take(k)
+        
+        .ToArray();
+
+        return gb;
+    }
+}
