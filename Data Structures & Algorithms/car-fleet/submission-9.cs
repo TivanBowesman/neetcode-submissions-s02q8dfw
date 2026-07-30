87fba462137186f6d321459290ba75c6c2ad5d90
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-07-30 23:29:37 +0100

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+        var arr = new List<(double time, int pos )>();
+        for (int i = 0; i < position.Length; i++){
+            var distance = target - position[i];
+            double time = (double)distance / speed[i]; 
+
+            arr.Add( (time, position[i]));
+        }
+        
+        //sort arr by time
+        arr.Sort((b, a) => a.pos.CompareTo(b.pos));
+        
+        int fleets = 0;
+        double max = double.MinValue;
+        foreach(var car in arr){
+            if(car.time > max){
+                fleets++; 
+                max = car.time;
+            }
+        }
+        return fleets; 
+    }
+}
+//if more than -> new fleet
+//cars match
+//speed = dist / time
+
+//time = dist / speed 
+//sort by pos
