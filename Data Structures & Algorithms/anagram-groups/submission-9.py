1701e58ca3e137a7c9832a20974ad288dbe4d8b9
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-08-02 00:57:51 +0100

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+
+        for s in strs:
+            sortWord = "".join(sorted(s))
+           
+            if sortWord not in map.keys():
+                map[sortWord] = [s]
+            else:
+                map[sortWord].append(s)
+
+        return list(map.values())
