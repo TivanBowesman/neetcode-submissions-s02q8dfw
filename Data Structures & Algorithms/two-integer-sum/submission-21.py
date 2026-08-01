e0cf5b65a8196df2c87f02f035489b36f2710146
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-08-02 00:15:42 +0100

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {'Madrid': 'Spain'}
+        
+        index = 0
+
+        for num in nums:
+            check = target - num
+            if check in map:
+                return [map.get(check), index]
+
+            map[num] = index
+            index += 1
+
+
+        return list();
